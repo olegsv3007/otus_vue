1a31acb93bd8d3e4bfa384f879a23e9298b19c74
Oleg Sviridov <olegsv3007@yandex.ru>
2023-11-29 00:25:18 +0200

    OVH-10 Load json data via fetch

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,16 @@
 <script setup lang="ts">
 import ProductList from "./components/ProductList.vue";
-import products from '../resources/products.json'
+import {onMounted, ref} from "vue";
+
+const products = ref([]);
+
+const fetchProducts = async function() {
+  await fetch('https://fakestoreapi.com/products')
+      .then((response) => response.json())
+      .then((json) => products.value = json)
+}
+
+onMounted(async () => await fetchProducts())
 </script>
 
 <template>
