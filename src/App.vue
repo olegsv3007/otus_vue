c1f35f99fe26043e2461428c61521a31e4c6bed3
Oleg Sviridov <olegsv3007@yandex.ru>
2023-11-29 00:53:50 +0200

    OVH-10 Add loading state

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,22 @@
 <script setup lang="ts">
 import ProductList from "./components/ProductList.vue";
-import {onMounted, ref} from "vue";
+import {onBeforeMount, onMounted, ref} from "vue";
 
 const products = ref([]);
+const isLoading = ref(true);
 
 const fetchProducts = async function() {
+  isLoading.value = true;
+
   await fetch('https://fakestoreapi.com/products')
       .then((response) => response.json())
       .then((json) => products.value = json)
+      .finally(() => setTimeout(() => isLoading.value = false, 1000));
 }
 
-onMounted(async () => await fetchProducts())
+onBeforeMount(async () => await fetchProducts())
 </script>
 
 <template>
-  <ProductList :products="products"/>
+  <ProductList v-if="products.length > 0" :products="products" :is-loading="isLoading"/>
 </template>
