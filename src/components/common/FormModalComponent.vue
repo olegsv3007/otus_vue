e6bfef48353d5fb161eefa0be621b7d3e22f3b82
Oleg Sviridov <olegsv3007@yandex.ru>
2024-02-03 00:22:15 +0200

    OVH-13 Create form modal component

---
+++ src/components/common/FormModalComponent.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <Dialog v-model:visible="visible" :header="header" modal>
+      <template #footer>
+        <Button label="Cancel" @click="closeModal" severity="secondary"/>
+        <Button label="Submit" @click="submit"/>
+      </template>
+    </Dialog>
+    <slot name="modal-initiator" :showModal="showModal"></slot>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Button from "primevue/button";
+import Dialog from 'primevue/dialog';
+import {Ref, ref} from "vue";
+
+interface Props {
+  header: string,
+}
+
+defineProps<Props>();
+
+let visible: Ref<boolean> = ref(false);
+
+const showModal = (): void => { visible.value = true };
+const closeModal = (): void => { visible.value = false };
+const submit = (): void => {
+  console.log('submit');
+  closeModal();
+};
+</script>
+
+<style scoped>
+
+</style>
