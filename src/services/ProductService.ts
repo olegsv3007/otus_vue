be7a34d9a2d5c0782e08e20ac3e95f1ebb94bd9f
Oleg Sviridov <olegsv3007@yandex.ru>
2024-02-11 20:03:53 +0200

    OVH-14 Use axios package to get products

--- src/services/ProductService.ts
+++ src/services/ProductService.ts
@@ -1,10 +1,11 @@
+import axios from "axios";
+
 export class ProductService {
     async fetchProducts(): Promise<Array<Product>> {
         let products: Array<Product> = [];
 
-        await fetch('https://fakestoreapi.com/products')
-            .then((response) => response.json())
-            .then((json) => products = json)
+        await axios.get('https://fakestoreapi.com/products')
+            .then((response) => products = response.data)
 
         return products;
     }
